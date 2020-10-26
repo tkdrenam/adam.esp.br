9a3c1eb22895bf59bbcf51623ae687cbb6c4ed03
Munir Ahmed Elsangedy <elsangedy@gmail.com>
2020-10-26 16:27:22 -0300

    fix: remove unused types

--- next-env.d.ts
+++ next-env.d.ts
@@ -2,19 +2,3 @@
 /// <reference types="next/types/global" />
 
 declare module '*.md'
-
-declare module '*.json' {
-  const value: any
-  export default value
-}
-
-declare module '*.yml' {
-  const value: any
-  export default value
-}
-
-declare global {
-  interface Window {
-    netlifyIdentity?: any
-  }
-}
