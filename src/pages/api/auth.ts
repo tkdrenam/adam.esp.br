941ad16184793c0c70a3cab8d3e1279217eebbac
Munir Ahmed Elsangedy <elsangedy@gmail.com>
2020-10-29 14:40:14 -0300

    feat: add internal admin auth

---
+++ src/pages/api/auth.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import crypto from 'crypto'
+import { create } from './_lib/oauth2'
+
+export const randomString = () => crypto.randomBytes(4).toString(`hex`)
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  const { host } = req.headers
+
+  const oauth2 = create()
+
+  const url = oauth2.authorizationCode.authorizeURL({
+    redirect_uri: `https://${host}/api/callback`,
+    scope: `repo,user`,
+    state: randomString(),
+  })
+
+  res.writeHead(301, { Location: url })
+  res.end()
+}
