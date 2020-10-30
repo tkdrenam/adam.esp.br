ce53623daa3fca7aee00d84395f44ed57377d861
Munir Ahmed Elsangedy <elsangedy@gmail.com>
2020-10-30 03:19:20 -0300

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 type Props = {
   title: string
@@ -11,87 +12,90 @@ type Props = {
 
 export const Header: React.FC<Props> = ({ title, contact }) => {
   return (
-    <header
-      style={{ height: 440, backgroundImage: 'url(/images/header.jpeg)' }}
-      className="bg-blue-500 bg-center bg-cover bg-no-repeat"
-    >
-      <div className="h-full flex flex-col justify-between bg-black bg-opacity-50">
-        <div className="container mx-auto py-10">
-          <div className="flex flex-row justify-between">
-            <div />
+    <header className="bg-gray-900 p-6">
+      <div className="container mx-auto">
+        <div className="flex flex-row justify-between">
+          <div className="flex items-center">
+            <Image
+              src="/images/icon.png"
+              width="160"
+              height="160"
+              alt="Logotipo Associação Duovizinhense de Artes Marciais"
+            />
+          </div>
 
-            <div className="flex flex-row">
+          <div className="flex flex-col justify-between">
+            <div className="lg:flex-1 flex flex-row justify-end pb-4 lg:pb-0">
               {contact.instagram && (
                 <a rel="nofollow" role="link" target="_blank" href={`https://www.instagram.com/${contact.instagram}`}>
-                  <img src="/icons/instagram.png" alt="Instagram" className="w-6 h-6" />
+                  <img src="/icons/instagram.png" alt="Instagram" className="w-4 h-4" />
                 </a>
               )}
               {contact.facebook && (
                 <a rel="nofollow" role="link" target="_blank" href={`https://www.facebook.com/${contact.facebook}`}>
-                  <img src="/icons/facebook.png" alt="Facebook" className="w-6 h-6 mx-4" />
+                  <img src="/icons/facebook.png" alt="Facebook" className="w-4 h-4 mx-4" />
                 </a>
               )}
               {contact.twitter && (
                 <a rel="nofollow" role="link" target="_blank" href={`https://www.twitter.com/${contact.twitter}`}>
-                  <img src="/icons/twitter.png" alt="Twitter" className="w-6 h-6" />
+                  <img src="/icons/twitter.png" alt="Twitter" className="w-4 h-4" />
                 </a>
               )}
             </div>
+
+            <div className="flex-1 flex items-center">
+              <nav className="flex-1">
+                <ul className="flex justify-center flex-wrap lg:-mr-3">
+                  <li>
+                    <Link href="/">
+                      <a className="text-white font-bold text-base uppercase px-2 py-6 mx-1">Home</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/historia">
+                      <a className="text-white font-bold text-base uppercase px-2 py-6 mx-1">O Clube</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/diretoria">
+                      <a className="text-white font-bold text-base uppercase px-2 py-6 mx-1">Taekwondo</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="#">
+                      <a className="text-white font-bold text-base uppercase px-2 py-6 mx-1">Transparência</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/blog">
+                      <a className="text-white font-bold text-base uppercase px-2 py-6 mx-1">Notícias</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="#">
+                      <a className="text-white font-bold text-base uppercase px-2 py-6 mx-1">Campeonatos</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="#">
+                      <a className="text-white font-bold text-base uppercase px-2 py-6 mx-1">Social</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/contato">
+                      <a className="text-white font-bold text-base uppercase px-2 py-6 mx-1">Contato</a>
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+
+            <div className="hidden lg:flex lg:flex-1" />
           </div>
         </div>
 
-        {/* <div className="container mx-auto sticky top-0 z-10"> */}
-        <div className="container mx-auto">
-          <nav className="bg-black bg-opacity-75 h-16">
-            <ul className="flex justify-between items-center h-full">
-              <li className="h-full">
-                <Link href="/">
-                  <a className="h-full flex items-center px-8 text-white font-bold text-base uppercase">HOME</a>
-                </Link>
-              </li>
-              <li className="h-full">
-                <Link href="/historia">
-                  <a className="h-full flex items-center px-8 text-white font-bold text-base uppercase">O CLUB</a>
-                </Link>
-              </li>
-              <li className="h-full">
-                <Link href="/diretoria">
-                  <a className="h-full flex items-center px-8 text-white font-bold text-base uppercase">TAEKWONDO</a>
-                </Link>
-              </li>
-              <li className="h-full">
-                <Link href="#">
-                  <a className="h-full flex items-center px-8 text-white font-bold text-base uppercase">
-                    TRANSPARÊNCIA
-                  </a>
-                </Link>
-              </li>
-              <li className="h-full">
-                <Link href="/blog">
-                  <a className="h-full flex items-center px-8 text-white font-bold text-base uppercase">NOTÍCIAS</a>
-                </Link>
-              </li>
-              <li className="h-full">
-                <Link href="#">
-                  <a className="h-full flex items-center px-8 text-white font-bold text-base uppercase">CAMPEONATOS</a>
-                </Link>
-              </li>
-              <li className="h-full">
-                <Link href="#">
-                  <a className="h-full flex items-center px-8 text-white font-bold text-base uppercase">SOCIAL</a>
-                </Link>
-              </li>
-              <li className="h-full">
-                <Link href="/contato">
-                  <a className="h-full flex items-center px-8 text-white font-bold text-base uppercase">CONTATO</a>
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-
         {title && (
-          <div className="container mx-auto py-12">
+          <div className="container mx-auto pt-8 pb-2">
             <h1 className="text-center font-bold text-4xl text-white uppercase">{title}</h1>
           </div>
         )}
