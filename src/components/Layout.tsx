8eb19995b64c22c2a05a382fc86c7ca5598ab124
Munir Ahmed Elsangedy <elsangedy@gmail.com>
2020-10-30 02:06:21 -0300

    add favicon

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,24 @@
 import Head from 'next/head'
 import { Header } from '@/components/Header'
 
-export const Layout = ({ children, pageTitle }) => {
+export const Layout = ({ children, contact, title }) => {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>{pageTitle}</title>
-        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        <title>{title}</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#2b5797" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
 
-      <Header title={pageTitle} />
+      <Header title={title} contact={contact} />
 
-      <main className="container mx-auto py-16">{children}</main>
+      <main className="md:container md:mx-auto py-16">{children}</main>
 
       <footer>Built by me!</footer>
     </>
